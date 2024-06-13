49249e22ac8fdfdb00151d0a43b820b6a2cdfc61
lkk143 <lamkimkhanh143@gmail.com>
2024-06-13 18:51:02 +0700

    Update init-db.sql

--- init-db.sql
+++ init-db.sql
@@ -1,12 +1,12 @@
-CREATE TABLE IF NOT EXISTS tblbookIUHX (
+CREATE TABLE IF NOT EXISTS tblbookIUH1465564 (
     id serial PRIMARY KEY,
     title varchar NOT NULL,
     author varchar NOT NULL
 );
 
 INSERT INTO tblbook (title, author) VALUES
-  ('DevOps', 'IUHX'),
-  ('Big Data', 'IUHX'),
-  ('Cloud Deployement', 'IUHX'),
-('Data Analysis', 'IUHX'),
-('Block Chain', 'IUHX');
+  ('DevOps', 'IUH1465564'),
+  ('Big Data', 'IUH1465564'),
+  ('Cloud Deployement', 'IUH1465564'),
+('Data Analysis', 'IUH1465564'),
+('Block Chain', 'IUH1465564');
